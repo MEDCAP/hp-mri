300c882a9e3a1b920040cc66624fafe65cd617b4
Kento Yamada <76908986+kekeke21@users.noreply.github.com>
2024-10-17 14:45:57 -0400

    Revert "Dev gitignore"

--- medcap-frontend/src/pages/RetrievePage.tsx
+++ medcap-frontend/src/pages/RetrievePage.tsx
@@ -1,18 +1,15 @@
 // src/pages/RetrievePage.tsx
 import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import { Link } from 'react-router-dom'; // Importing Link to handle navigation
-import HeaderAccount from '../components/HeaderAccount';
 import './../styles/pages.css';
-import './../styles/retrieve.css';
 
 const RetrievePage: React.FC = () => {
   const [search, setSearch] = useState('');
 
   const dataList = [
-    'MEDCAP | Sequence 1 | Date',
-    'MEDCAP | Sequence 2 | Date',
-    'MEDCAP | Sequence 3 | Date',
+    'MEDCAP-01 | Sequence 1 | Date',
+    'MEDCAP-02 | Sequence 2 | Date',
+    'MEDCAP-03 | Sequence 3 | Date',
+    // Add more data as needed
   ];
 
   const filteredData = dataList.filter((item) =>
@@ -20,32 +17,20 @@ const RetrievePage: React.FC = () => {
   );
 
   return (
-    <div className="page-container">
-      <HeaderAccount />
-      <Sidebar />
-      <div className="retrieve-content">
-        {/* Upload button on the top right */}
-        <div className="top-right">
-          <Link to="/upload">
-            <button className="button primary">Upload</button>
-          </Link>
-        </div>
-        <div className="retrieve-container">
-          <h1>Retrieve Data</h1>
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <ul className="data-list">
-            {filteredData.map((item, index) => (
-              <li key={index} className="data-item">{item}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div className="retrieve-container">
+      <h1>Retrieve Data</h1>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul className="data-list">
+        {filteredData.map((item, index) => (
+          <li key={index} className="data-item">{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
