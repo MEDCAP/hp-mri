b2c553b0fd7a290b907a35a52507eaf2eab10bca
shurikzav <30868829+shurikzav@users.noreply.github.com>
2025-06-01 23:17:37 -0400

    played with comments

--- hp-mri-frontend/src/pages/calculator/MRCalculatorPage.tsx
+++ hp-mri-frontend/src/pages/calculator/MRCalculatorPage.tsx
@@ -209,22 +209,4 @@ const MRCalculatorPage: React.FC = () => {
 
 export default MRCalculatorPage;
 
-// import { useEffect } from 'react';
-// import React from "react";
-// import InputForm from "./components/InputForm";
-// import styles from "./MRCalculator.module.css";
-
-// const MRCalculatorPage: React.FC = () => {
-//     useEffect(() => {
-//         document.title = "HP-MRI MR Calculator"; // Dynamically updates the tab title
-//     }, []);
-
-//     return (
-//         <div className={styles.container}>
-//             <h1 className={styles.title}>MR Coil Component Calculator</h1>
-//             <InputForm />
-//         </div>
-//     );
-// };
-
-// export default MRCalculatorPage;
+// comment
