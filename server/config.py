44bdf6ee13bad4fe205537e5692da615626b1eb3
kekeke21 <kentoyamada2001@gmail.com>
2025-06-24 18:09:43 -0400

    use .env.development based on .env.sample to pass aws credentials to dev stage

--- server/config.py
+++ server/config.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
     # parse aws credentials from ~/.aws/credentials after aws-federated-login
-    FLASK_APP='app.py'
+    FLASK_APP='run.py'
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
+    load_dotenv('.env.development')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')
     CORS_ORIGINS = ['http://localhost:5173']    # frontend serve dist folder
 
 class ProductionConfig(Config):
