0443e47360ac959ed60b3a2a2a999b0fa11a76a0
kekeke21 <kentoyamada2001@gmail.com>
2025-07-31 15:13:53 -0400

    access mongodb using aws IAM role generated with aws-federated-auth command - login aws-federated-auth - aws configure export-credentials --profile aws-medcap-psom-PennResearcher --format env-no-export > .env.development - config.py constructs MONGO_URI using obtained aws credentials and export to test_data.py

--- server/config.py
+++ server/config.py
@@ -1,17 +1,20 @@
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 class Config:
-    # parse aws credentials from ~/.aws/credentials after aws-federated-login
     FLASK_APP='run.py'
 
 class DevelopmentConfig(Config):
-    DEBUG=True
+    DEBUG=True    
+    # parse aws credentials from ~/.aws/credentials after aws-federated-login
     load_dotenv('.env.development')
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')
     CORS_ORIGINS = ['http://localhost:5173']    # frontend serve dist folder
+    MONGO_URI = f'mongodb+srv://{quote_plus(AWS_ACCESS_KEY_ID)}:{quote_plus(AWS_SECRET_ACCESS_KEY)}@mrd-files.gzajigq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:{quote_plus(AWS_SESSION_TOKEN)}&appName=mrd-files'
 
 class ProductionConfig(Config):
     DEBUG=False
+    # MONGO_URI for?
--- server/test_data.py
+++ server/test_data.py
@@ -1,22 +1,15 @@
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import os
-import json
-from bson import json_util  # For properly handling MongoDB's ObjectId
+from config import DevelopmentConfig
 
-load_dotenv('.env.database')
-MONGO_URI = os.getenv('MONGO_URI')
+# load_dotenv('.env.database')
+MONGO_URI = DevelopmentConfig.MONGO_URI
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(host=MONGO_URI)
 db = client.get_database("medcap_dev")
 mrdfiles = db.get_collection("mrdfiles")
 
-# Method 1: Convert cursor to list and print all documents
-# documents = list(mrdfiles.find({}))
-# result = json.dumps(documents, default=json_util.default, indent=2)
-print(list(mrdfiles.find()))
-for doc in mrdfiles.find():
-    print(doc['_id'])
-    print(doc['file_name'])
+print(list(mrdfiles.find({})))
 
 client.close()
