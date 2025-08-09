1f756f1a572d840b065d117f4e8fae98d400131a
kekeke21 <kentoyamada2001@gmail.com>
2025-08-09 17:15:49 -0400

    aws envrionment variables from env files was supposed to be capitalized for linux environment

--- hp-mri-frontend/vite.config.ts
+++ hp-mri-frontend/vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // command: 'serve' or 'build' based on npm run command
 // mode: 'development' or 'production' based on npm 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react()],
     // redirect any /api requests to API_URL specified in .env.development
     server: mode === 'development' ? {
         proxy: {
           '/api': {
-            target: 'http://localhost:5000',
+            target: 'http://localhost:8000',
             changeOrigin: true,
             secure: false
           }
