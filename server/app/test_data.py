021d8dddb7825637601a570f477d8052d430e862
kekeke21 <kentoyamada2001@gmail.com>
2025-08-13 11:50:26 -0400

    new sample mrd file with acq and image field included

---
+++ server/app/test_data.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+# add filepath of parent directory
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[1]
+sys.path.insert(0, str(path_root))
+
+import app.external.python.mrd as mrd
+
+def read_mrd():
+    filename = "images.bin"
+    with mrd.BinaryMrdReader(filename) as r:
+        head = r.read_header()
+        data_stream = r.read_data()
+        acq_counter = 0
+        img_counter = 0
+        wf_counter = 0
+
+        for item in data_stream:
+            if isinstance(item, mrd.StreamItem.Acquisition):
+                acq_counter += 1
+                print("acquisition", item.value.data.shape)
+            elif isinstance(item, mrd.StreamItem.ImageFloat):
+                img_counter += 1
+                print("image", item.value.data.shape)
+            elif isinstance(item, mrd.StreamItem.WaveformUint32):
+                wf_counter += 1
+                print("waveform", item.value.data.shape)
+        print("acq_counter", acq_counter)
+        print("img_counter", img_counter)
+        print("wf_counter", wf_counter)
+
+
+if __name__ == "__main__":
+    read_mrd()
